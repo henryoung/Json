f577937d6a0a5ee3381758b3f112088bb7594ce9
henryoung <keyuyang@163.com>
2015-08-07 12:35:05 +0800

    using streaming model to parse json data

--- TestJsonParse.java
+++ TestJsonParse.java
@@ -1,11 +1,23 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
+
+
+import jsonparseutil.JsonParseExUtilities;
+import jsonparseutil.JsonParseUtilities;
+import jsonparseutil.MatchRule;
 
 /**
  * This program parse json data from a local file.
@@ -16,6 +28,7 @@ public class TestJsonParse
 {
 	   public static void main(String[] args)
 	   {
+		   
 		   if (args.length != 2  && args.length != 0)
 		   {
 			   System.out.println("Unrecognized Input Param!\nPlease Input 2 Params:\nFirst is Json Data File to be Parsed\nSecond is Json Rule File to be Applied for Parse");
@@ -55,13 +68,18 @@ public class TestJsonParse
 			   System.out.println(e.toString());			
 		   }
 		   
+		   long startTime = 0;
+		   long endTime = 0;	   
+	       
+		   startTime =  System.currentTimeMillis();
+		   System.out.println("=======Json Parse Method 1 Start=======");
+		   
 		   search_node_name = searchRule.getSEARCH_NODE_NAME();
 		   jsonMatchRuleMap = searchRule.getMATCH_RULEE();
 		   jsonActionRuleMap = searchRule.getACTION_RULEE();
 		   countRuleKey = searchRule.getCOUNT_RULE_KEY();
 		   countRuleValue = searchRule.getCOUNT_RULE_VALUE();
 		   		   
-		   System.out.println("=======");
 		   System.out.println("Search Node Name : " + search_node_name);
 		   System.out.println("Macth Rule : " + jsonMatchRuleMap);
 		   System.out.println("Action Rule : " + jsonActionRuleMap);
@@ -79,14 +97,58 @@ public class TestJsonParse
 		   {
 			   System.out.println("SubNode is not exist!");
 		   }
-		   
-		   System.out.println("=======");		   
+		   	   
 		   System.out.println("Count Rule : " + countRuleKey + " = " + countRuleValue);
 		   int elementNum = JsonParseUtilities.countElement(jElmList, countRuleKey, countRuleValue);
 		   System.out.println("Conuted Num = " + elementNum);
-		   System.out.println("=======");	   
+		   System.out.println("Output Parsed Data to " + jsonActionRuleMap.get("OUT_FILE") + "!");
+		   
+		   System.out.println("=======Json Parse Method 1 End=======");
+		   endTime = System.currentTimeMillis();
+		   System.out.println("Json Parse Method 1 Excute Time : " + (float)(endTime-startTime)/1000);
+		   
+		   startTime =  System.currentTimeMillis();
+		   System.out.println("=======Json Parse Method 2 Start=======");
+		   try
+		   {
+			   Reader in = new FileReader("responseDetailsOfSalesorderItem.json");
+			   //Writer out = new OutputStreamWriter(System.out, "UTF-8");
+			   Writer out = new FileWriter("out.json");
+			   MatchRule test = new MatchRule();
+			   test.setPARTN_ROLE("AG");
+			   test.setSD_DOC("0000000151");
+			   AbstractMap.SimpleImmutableEntry<String, Integer> pair = new AbstractMap.SimpleImmutableEntry<>("LEVEL_NR", 100);
+			   ArrayList<JsonObject> jObjList = JsonParseExUtilities.GsonStreamParse(in, out, "ORDER_PARTNERS_OUT", test, pair);
+			   System.out.println(jObjList.toString());
+			   int countedNum = 0;
+			   for (JsonObject temp : jObjList)
+			   {
+				   JsonElement jElm = temp.get("SD_DOC");
+				   if (jElm != null)
+				   {
+					   String value = jElm.getAsString();
+					   String regx = "0000000151";
+					   if (value.equals(regx))
+					   {
+						   ++countedNum;
+					   }
+				   }				   
+			   }
+			   System.out.println(countedNum);
+			   System.out.println("Output Parsed Data to out.json!");
+		   }
+		   catch (IOException e)
+		   {
+			   e.printStackTrace();
+				   
+			   System.out.println(e.toString());			
+		   }
+		   
+		   System.out.println("=======Json Parse Method 2 End=======");
+		   endTime = System.currentTimeMillis();
+		   System.out.println("Json Parse Method 2 Excute Time : " + (float)(endTime-startTime)/1000);
 	   }
-	   
+		   	   
 	   /**
 	    * This class help parse search rule file formatted as json.
 	    * @version 1.0 2015-08-05
